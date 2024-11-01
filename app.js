91931ee2a7cf79106abf837017e68fbef0b98103
Denis Mwangi <deniskimani2022@gmail.com>
2024-11-01 14:29:54 +0300

    i was able to create user registration and login

--- app.js
+++ app.js
@@ -1,5 +1,107 @@
-//require the dbConnect function
+// external imports
+const express = require("express");
 const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("./auth");
 
-//call the dbConnect function
+// initialize express app
+const app = express();
+
+// Connect to MongoDB
 dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Registration endpoint
+app.post("/register", async (req, res) => {
+  const { email, username, password } = req.body;
+
+  // Validate request data
+  if (!email || !username || !password) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
+
+  try {
+    // Check if the user already exists and return an error message if the user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "User with this email already exists!" });
+    }
+
+    //hash the password using bcryptjs by passing the password and the number of rounds to hash the password(salt)
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user to save in the database, by passing the email, username and hashed password
+    const newUser = new User({ email, username, password: hashedPassword });
+    await newUser.save();
+
+    // Respond with success message if user is created successfully
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user", error });
+  }
+});
+
+// Login endpoint
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validate request data
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Email and Password are required!" });
+  }
+
+  try {
+    // Check if the user exists and return an error message if the user does not exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    // Compare the password provided with the hashed password in the database
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+
+    // Return an error message if the password is incorrect
+    if (!isPasswordMatching) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    // Create a JWT token if the password is correct
+    const token = jwt.sign({ email: user.email }, "secret", {
+      expiresIn: "24h",
+    });
+
+    // Respond with the token if the user is logged in successfully
+    res.status(200).json({ message: "Login successful!", token: token });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in user", error });
+  }
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", auth, (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
+
+module.exports = app;
---
+++ db/userModel.js
@@ -0,0 +1,25 @@
+//require mongoose
+const mongoose = require("mongoose");
+
+//create a new user schema
+const userSchema = new mongoose.Schema({
+  //we will require email, username and password for user registration
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email already exists!"],
+  },
+  username: {
+    type: String,
+    required: [true, "Please provide a Username!"],
+    unique: [true, "Username already exists!"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a Password!"],
+    unique: false,
+  },
+});
+
+//export the user schema model
+module.exports = mongoose.model.Users || mongoose.model("User", userSchema);
