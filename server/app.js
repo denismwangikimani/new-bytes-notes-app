752938240dc145178861912056e1a52ec6c71a4b
Denis Mwangi <deniskimani2022@gmail.com>
2024-11-05 12:05:54 +0300

    fix: assign default title for notes if not provided

--- server/app.js
+++ server/app.js
@@ -127,7 +127,7 @@ app.post("/notes", auth, async (req, res) => {
   let { title, content } = req.body;
 
   // If title or content is blank, assign a default value or return an error
-  if (!title) title = "";
+  if (!title) title = "Untitled Note";
   if (!content) content = "";
 
   try {
