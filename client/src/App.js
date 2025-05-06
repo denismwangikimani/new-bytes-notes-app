25c45d76eca0ee92677b0317ee56b2db97018e9f
Denis Mwangi <deniskimani2022@gmail.com>
2025-05-06 12:14:40 +0300

    feat: improved App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/payment-confirmation" element={<PaymentConfirmation />} />
         {/* Protect the Notes route */}
         <Route
           path="/notes"
@@ -23,7 +24,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/payment-confirmation" element={<PaymentConfirmation />} />
       </Routes>
     </Router>
   );
